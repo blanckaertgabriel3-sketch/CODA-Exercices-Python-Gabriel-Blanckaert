0870ec5ea60adf9afc7fdb1e3a6338834b183f05
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2025-10-02 13:37:44 +0200

    exercice2

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def exercice1():
     print("Exercice 1 : Bonjour le monde !")
     print("Hello World !")
 
+def exercice2():
+    print("Exercice 2 : Afficher prenom.")
+    prenom = input ("entrer votre prénom")
+    print("bonjour, {prenom} !")
+
+
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
