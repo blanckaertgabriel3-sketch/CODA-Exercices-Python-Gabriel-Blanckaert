532b29a7e19d24ef58211ec7293d7e3d670b3b39
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2025-10-02 14:03:47 +0200

    exercice3

--- main.py
+++ main.py
@@ -9,15 +9,25 @@ def exercice2():
 
 def exercice3():
     print("Exercice 2 : afficher message sur 3 lignes")
-    print(Première ligne)\n
-    print(Deuxième ligne)\n
-    print(Troisième ligne)\n
+    print("Première ligne\n")
+    print("Deuxième ligne\n")
+    print("Troisième ligne\n")
+
+def exercice4():
+    print("Exercice 2 : Calculer l'âge")
+
+
 
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
     if choix == "1":
         exercice1()
+    elif == "2":
+        exercice2()
+    elif == "3":
+        exercice3()
+   
     else:
         print("Exercice non reconnu.")
 if __name__ == "__main__":
